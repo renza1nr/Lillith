6378c7543e32feb8788c13d09a2cd7c5a4db2f8d
renza1nr <breezefresco@gmail.com>
2026-07-07 22:05:20 +1000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -9,7 +9,7 @@ export const botConfig = {
   // - "idle"      = yellow moon
   // - "dnd"       = red do-not-disturb
   // - "invisible" = appears offline
-  presence: {10
+  presence: {
     // Current online state shown on Discord.
     status: "dnd",
 
