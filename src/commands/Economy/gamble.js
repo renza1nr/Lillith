bd1298884f4d660d0c2f0427ac28a529ce75b042
renza1nr <breezefresco@gmail.com>
2026-07-09 03:05:54 +1000

    Update gamble.js

--- src/commands/Economy/gamble.js
+++ src/commands/Economy/gamble.js
@@ -8,7 +8,7 @@ const BASE_WIN_CHANCE = 0.4;
 const CLOVER_WIN_BONUS = 0.1;
 const CHARM_WIN_BONUS = 0.08;
 const PAYOUT_MULTIPLIER = 2.0;
-const GAMBLE_COOLDOWN = 5 * 60 * 1000;
+const GAMBLE_COOLDOWN = 3 * 20 * 100;
 
 export default {
     data: new SlashCommandBuilder()
